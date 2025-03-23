ab786d4ea8b316233ec907819ed60ec5a9f445e2
Simran0331 <156924230+Simran0331@users.noreply.github.com>
2025-03-23 17:50:18 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Avatar from "react-avatar";
-
 const Navbar = () => {
   const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
